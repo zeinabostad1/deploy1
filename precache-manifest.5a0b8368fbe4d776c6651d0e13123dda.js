34601473754827d38c90e0beb53ea3a4d6f76c93
zeinabostad1 <70534881+zeinabostad1@users.noreply.github.com>
2020-09-02 15:09:34 -0400

    Updates

---
+++ precache-manifest.5a0b8368fbe4d776c6651d0e13123dda.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c6512656ee0a668366721ef6c4dbec93",
+    "url": "/deploy1/index.html"
+  },
+  {
+    "revision": "35e673539dd2ee618be7",
+    "url": "/deploy1/static/css/main.913952d6.chunk.css"
+  },
+  {
+    "revision": "7f6532b4dd22dd46b4e6",
+    "url": "/deploy1/static/js/2.65c08628.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/deploy1/static/js/2.65c08628.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "35e673539dd2ee618be7",
+    "url": "/deploy1/static/js/main.48da0c5b.chunk.js"
+  },
+  {
+    "revision": "898529662a2c1721f576",
+    "url": "/deploy1/static/js/runtime-main.f67fc5ee.js"
+  }
+]);
